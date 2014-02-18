884a92b718c10339d802874f3d719dba72b4779e
Jonas A. Hultén <jon.hulten@gmail.com>
2014-02-18 21:02:31 +0100

    Willingly destroyed everything so I can rebuild and perhaps get a better setup.

--- init.el
+++ init.el
@@ -1,14 +1,14 @@
 (custom-set-variables
-  ;; custom-set-variables was added by Custom.
-  ;; If you edit it by hand, you could mess it up, so be careful.
-  ;; Your init file should contain only one such instance.
-  ;; If there is more than one, they won't work right.
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
  '(inhibit-startup-screen t))
 (custom-set-faces
-  ;; custom-set-faces was added by Custom.
-  ;; If you edit it by hand, you could mess it up, so be careful.
-  ;; Your init file should contain only one such instance.
-  ;; If there is more than one, they won't work right.
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
  )
 
 ;; DrSLDR Emacs config
@@ -17,7 +17,7 @@
 ;; Load path setting to allow packages to be loaded
 (let ((default-directory
   (concat user-emacs-directory
-    (convert-standard-filename "packs/"))))
+    (convert-standard-filename "manual/"))))
   (setq load-path
     (append
       (let ((load-path (copy-sequence load-path))) ;; Shadow
@@ -26,14 +26,27 @@
           (normal-top-level-add-subdirs-to-load-path)))
       load-path)))
 
+;; Test the custom libraries
+;; Whine loudly if they aren't there
+(when (require 'better-defaults nil 'noerror) message "Better
+  Defaults missing. Entire thing (likely) bork'd. Re-clone
+  repo.")
+
 ;; Load the custom libraries
 (load-library "better-defaults")
 (load-library "color-theme")
 
+;; Configure Marmalade for package management
+(require 'package)
+(add-to-list 'package-archives
+             '("marmalade" . "http://marmalade-repo.org/packages/"))
+(package-initialize)
+
 ;; Enable line wrapping at column 80 globally
-(setq-default fill-column 80)
+
 (defun auto-fill-turn-on ()
-  (auto-fill-mode 1))
+  (auto-fill-mode 1)
+  (setq-default fill-column 80))
 (add-hook 'find-file-hooks 'auto-fill-turn-on)
 
 ;; Enable line and column numbering
