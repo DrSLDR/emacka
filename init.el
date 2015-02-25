7f00529771992503a8885cdbf16735d72f5e83e2
Jonas A. Hultén <jon.hulten@gmail.com>
2015-02-25 22:30:07 +0100

    Added offscreen parenthesis finding

--- init.el
+++ init.el
@@ -145,6 +145,20 @@
 
 (add-hook 'doc-view-mode-hook 'auto-revert-mode)
 
+;;;;;;;;;;;;;;;;;;;;; Show offscreen matching parenthesis ;;;;;;;;;;;;;;;;;;;;;;
+
+(defadvice show-paren-function
+  (after show-matching-paren-offscreen activate)
+  "If the matching paren is offscreen, show the matching line in the
+  echo area. Has no effect if the character before point is not of
+  the syntax class ')'."
+  (interactive)
+  (let* ((cb (char-before (point)))
+    (matching-text (and cb
+      (char-equal (char-syntax cb) ?\) )
+      (blink-matching-open))))
+    (when matching-text (message matching-text))))
+
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; END ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
